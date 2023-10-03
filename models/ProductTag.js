35276367b71423747d6aeb66e743983f8b1162c3
Chad Smith <cds021@yahoo.com>
2023-10-03 10:04:11 -0400

    updating models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,30 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER, 
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // define product_id
+    product_id: {
+      type: DataTypes.INTEGER,
+      // references the product model's id 
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    // defines tag_id
+    tag_id: {
+      type: DataTypes.INTEGER,
+      // references the tag model's id 
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
